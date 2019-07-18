165521dd84e1be1ffbbfe7b9f62c84c395d5cdb9
Colton Kayser <coltonkayser@gmail.com>
2019-07-18 12:17:36 -0400

    added more customization to randomizer

--- app.js
+++ app.js
@@ -20,22 +20,34 @@ $(() => {
     // console.log($lastName);
 
 
-
-
+    const $jokeRandomizer = Math.floor(Math.random() * 550);
+    console.log($jokeRandomizer);
 
 
   $.ajax({
-       url:'https://api.icndb.com/jokes/random?firstName=' +$firstName+ '&lastName=' +$lastName+ '&exclude=[explicit]'
+       url:'https://api.icndb.com/jokes/'+$jokeRandomizer+'+'?firstName=' +$firstName+ '&lastName=' +$lastName+ '&exclude=[explicit]'
    }).then(
        (data)=>{
          ///get joke
            let $joke = data.value.joke;
            ////create div to store joke
-           let $jokeContainer = $('<div>');
+           let $jokeContainer = $('<div>').addClass('joke-container');
            ///add joke div to DOM
            $('body').append($jokeContainer);
            ///add joke to joke div
            $($jokeContainer).append($joke);
+
+           const $moreJokesbutton = $('<button>').text('Another Joke').addClass('more-jokes-button');
+           $($jokeContainer).append($moreJokesbutton);
+
+           
+
+
+
+
+
+
+
        },
        ()=>{
            console.log('bad request');

9411c013fee3e5a0ade24dba2a8509d8368a5922
Colton Kayser <coltonkayser@gmail.com>
2019-07-18 14:13:37 -0400

    testing github connection

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 $(() => {
 //////get user input on button click
-  $('#norrized-button').on('click', (event) => {
+  $('.norrized-button').on('click', (event) => {
     /////prevent refresh
     event.preventDefault();
     /////get user input for first name
@@ -20,12 +20,12 @@ $(() => {
     // console.log($lastName);
 
 
-    const $jokeRandomizer = Math.floor(Math.random() * 550);
-    console.log($jokeRandomizer);
+    // const $jokeRandomizer = Math.floor(Math.random() * 550);
+    // console.log($jokeRandomizer);
 
 
   $.ajax({
-       url:'https://api.icndb.com/jokes/'+$jokeRandomizer+'+'?firstName=' +$firstName+ '&lastName=' +$lastName+ '&exclude=[explicit]'
+      url:'https://api.icndb.com/jokes/random?firstName=' +$firstName+ '&lastName=' +$lastName+ '&exclude=[explicit]'
    }).then(
        (data)=>{
          ///get joke
@@ -33,14 +33,17 @@ $(() => {
            ////create div to store joke
            let $jokeContainer = $('<div>').addClass('joke-container');
            ///add joke div to DOM
-           $('body').append($jokeContainer);
+           $('#whole-container').append($jokeContainer);
            ///add joke to joke div
            $($jokeContainer).append($joke);
 
-           const $moreJokesbutton = $('<button>').text('Another Joke').addClass('more-jokes-button');
-           $($jokeContainer).append($moreJokesbutton);
+           // const $moreJokesbutton = $('<button>').text('Another Joke').addClass('norrized-button');
+           // $('body').append($moreJokesbutton);
 
-           
+            // $moreJokesbutton.on('click', () => {
+            //   // const $jokeRandomizer = Math.floor(Math.random() * 550);
+            //   $($jokeContainer).append($joke);
+            // })
 
 
 

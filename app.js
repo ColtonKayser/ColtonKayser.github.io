3f5e18aa3dfc66952548b02d630035b83f9d6a68
Colton Kayser <coltonkayser@gmail.com>
2019-07-18 11:08:05 -0400

    got inputs working and set default name to Chuck Norris

--- app.js
+++ app.js
@@ -1,10 +1,39 @@
 $(() => {
+//////get user input on button click
+  $('#norrized-button').on('click', (event) => {
+    /////prevent refresh
+    event.preventDefault();
+    /////get user input for first name
+    let $firstName = $('#first-name').val();
+    ///sets default first name as Chuck
+    if ($firstName == ''){
+      $firstName = 'Chuck';
+    }
+    console.log($firstName);
+
+    ////get user input for last name
+    let $lastName = $('#last-name').val();
+    ///sets default last name as Norris
+    if ($lastName == ''){
+      $lastName = 'Norris';
+    }
+    console.log($lastName);
+
+
+
+
+
+  })
+
+
+
+
 
   $.ajax({
        url:'https://api.icndb.com/jokes/random?firstName=Colton&lastName=Kayser&exclude=[explicit]'
    }).then(
        (data)=>{
-           console.log(data.value.joke);
+           // console.log(data.value.joke);
        },
        ()=>{
            console.log('bad request');

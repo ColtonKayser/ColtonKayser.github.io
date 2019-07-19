991bd3755af3b8017496efe998195a2cccfb34de
Colton Kayser <coltonkayser@gmail.com>
2019-07-18 21:43:43 -0400

    next button on carousel works

--- app.js
+++ app.js
@@ -1,4 +1,37 @@
 $(() => {
+
+  //Keep track of current image index
+  let $currentImgIndex = 0;
+  // grab current image element
+  let $currentImg= $('.carousel-images').children().eq($currentImgIndex);
+  ////hide all the images on init
+  $('.carousel-images').children().hide();
+  ////show first image on init
+  $('.carousel-images').children().eq(0).show();
+  /////total number of images
+  let $numberOfImages = $('.carousel-images').children().length - 1;
+  console.log($numberOfImages);
+
+
+  const $next = $('.next-button');
+
+  $next.on('click', () => {
+    $currentImg.hide();
+
+    if($currentImgIndex < $numberOfImages) {
+      $currentImgIndex++;
+    } else {
+      $currentImgIndex = 0;
+    }
+    $currentImg = $('.carousel-images').children().eq($currentImgIndex);
+    $currentImg.show();
+  });
+
+
+
+
+
+
 //////get user input on button click
   $('.norrized-button').on('click', (event) => {
     /////prevent refresh
